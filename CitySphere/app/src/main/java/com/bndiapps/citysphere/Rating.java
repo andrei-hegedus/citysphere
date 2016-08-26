1da46f6d6c5db044c35decb41e30693bb7d9a225
Andrei Hegedus-Sirbu <andrei.hegedus-sirbu@iquestgroup.com>
2016-08-26 10:37:08 +0300

    Project skeleton

---
+++ CitySphere/app/src/main/java/com/bndiapps/citysphere/Rating.java
@@ -0,0 +1,10 @@
+package com.bndiapps.citysphere;
+
+import android.provider.BaseColumns;
+import com.google.android.gms.maps.model.LatLng;
+
+public class Rating implements BaseColumns{
+  private LatLng location;
+  private float rating;
+
+}
